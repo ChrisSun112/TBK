4aedb2dd9927e9378b77d33dfde99a01d4f0bc6c
徐航 <>
2018-11-08 22:19:18 +0800

    使用Senparc类库重新微信处理事件

---
+++ Softcomm.Weixin.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Softcomm.Weixin.Web.MvcApplication" Language="C#" %>
---
+++ Softcomm.Weixin.Web/Global.asax.cs
@@ -0,0 +1,84 @@
+﻿using Senparc.CO2NET;
+using Senparc.CO2NET.RegisterServices;
+using Senparc.Weixin;
+using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Softcomm.Weixin.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //设置全局 Debug 状态
+            var isGLobalDebug = true;
+            //全局设置参数，将被储存到 Senparc.CO2NET.Config.SenparcSetting
+            var senparcSetting = SenparcSetting.BuildFromWebConfig(isGLobalDebug);
+            //也可以通过这种方法在程序任意位置设置全局 Debug 状态：
+            //Senparc.CO2NET.Config.IsDebug = isGLobalDebug;
+
+
+            //CO2NET 全局注册，必须！！
+            IRegisterService register = RegisterService.Start(senparcSetting).UseSenparcGlobal();
+
+
+            #region 注册日志（按需，建议）
+
+            register.RegisterTraceLog(ConfigWeixinTraceLog);//配置TraceLog
+
+            #endregion
+
+
+            /* 微信配置开始
+             * 建议按照以下顺序进行注册
+             */
+
+            //设置微信 Debug 状态
+            var isWeixinDebug = true;
+            //全局设置参数，将被储存到 Senparc.Weixin.Config.SenparcWeixinSetting
+            var senparcWeixinSetting = SenparcWeixinSetting.BuildFromWebConfig(isWeixinDebug);
+            //也可以通过这种方法在程序任意位置设置微信的 Debug 状态：
+            //Senparc.Weixin.Config.IsDebug = isWeixinDebug;
+
+            //微信全局注册，必须！！
+            register.UseSenparcWeixin(senparcWeixinSetting, senparcSetting)
+
+        }
+
+        /// <summary>
+        /// 配置微信跟踪日志
+        /// </summary>
+        private void ConfigWeixinTraceLog()
+        {
+            //Senparc.CO2NET.Config.IsDebug = false;
+
+            //这里设为Debug状态时，/App_Data/WeixinTraceLog/目录下会生成日志文件记录所有的API请求日志，正式发布版本建议关闭
+            Senparc.Weixin.WeixinTrace.SendCustomLog("系统日志", "系统启动");//只在Senparc.Weixin.Config.IsDebug = true的情况下生效
+
+            //自定义日志记录回调
+            Senparc.Weixin.WeixinTrace.OnLogFunc = () =>
+            {
+                //加入每次触发Log后需要执行的代码
+            };
+
+            //当发生基于WeixinException的异常时触发
+            Senparc.Weixin.WeixinTrace.OnWeixinExceptionFunc = ex =>
+            {
+                //加入每次触发WeixinExceptionLog后需要执行的代码
+
+                //发送模板消息给管理员
+                var eventService = new EventService();
+                eventService.ConfigOnWeixinExceptionFunc(ex);
+            };
+        }
+    }
+}
